ff7cd040c41de43ea7dfa03fa568a174e57eaee3
HenokG7 <henoktesfayeg7@gmail.com>
2024-09-15 11:24:39 +1200

    4th project

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const bodyParser= require("body-parser")
+const multer=require("multer");
+let upload=multer({ dest: './public/' })
 var app = express();
-
 app.use(cors());
+//app.use(bodyParser.urlencoded({extended: false}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
+//upfile is the name that is used in the input field name 
+app.post("/api/fileanalyse",upload.single('upfile'),async(req,res)=>{
+ res.json({
+  name:req.file.originalname,
+  type:req.file.mimetype,
+  size:req.file.size
+ })
+})
 
 
 const port = process.env.PORT || 3000;
